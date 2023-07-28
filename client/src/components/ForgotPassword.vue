5a238d7b111fa471da4dc89092f530a7410f267e
Zakaria AIDOUNI <zakariaaidouni@gmail.com>
2023-07-28 12:57:12 +0200

    customer side optimization

--- client/src/components/ForgotPassword.vue
+++ client/src/components/ForgotPassword.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="forgot-password-container">
-    <input v-model="user.email" type="email" placeholder="Email">
-    <button @click="forgotPassword">Réinitialiser le mot de passe</button>
+  <div className="forgot-password-container">
+    <input v-model.trim="user.email" type="email" placeholder="Email">
+    <button :disabled="isLoading" @click="forgotPassword">Réinitialiser le mot de passe</button>
 
-    <!-- Ajoutez votre loader ici. Il sera affiché lorsque isLoading est true. -->
     <div v-if="isLoading">Loading...</div>
-
-    <!-- Message d'erreur ici -->
     <div v-if="errorMessage">{{ errorMessage }}</div>
   </div>
 </template>
@@ -26,18 +23,25 @@ export default {
   },
   methods: {
     async forgotPassword() {
-      this.isLoading = true;
-      try {
-        const response = await axios.post(`${process.env.VUE_APP_API_URL}/api/v1/users/forgotPassword`, this.user);
-        if (response.status === 200) {
-          console.log(response.data);
-          this.errorMessage = 'Un lien de réinitialisation de mot de passe a été envoyé à votre adresse e-mail.';
-        }
-      } catch (error) {
-        this.errorMessage = error.response.data.message || 'Une erreur inconnue est survenue.';
-      } finally {
-        this.isLoading = false;
+      if (!this.user.email) {
+        this.errorMessage = 'Veuillez entrer un email valide.';
+        return;
       }
+
+      this.isLoading = true;
+      this.errorMessage = '';
+
+      axios.post(`${process.env.VUE_APP_API_URL}/api/v1/users/forgotPassword`, this.user)
+          .then(response => {
+            console.log(response.data);
+            this.errorMessage = 'Un lien de réinitialisation de mot de passe a été envoyé à votre adresse e-mail.';
+          })
+          .catch(error => {
+            this.errorMessage = error.response?.data?.message || 'Une erreur inconnue est survenue.';
+          })
+          .finally(() => {
+            this.isLoading = false;
+          });
     },
   },
 };
