a6e01662cb2d419d223f321c68a2017017cdad83
Zakaria AIDOUNI <zakariaaidouni@gmail.com>
2023-07-28 13:42:50 +0200

    centralize error handling

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -6,33 +6,30 @@ const UserMg = require('../db/mongo/models/userModel')
 const HandMg = require('../db/mongo/models/handModel')
 const GameMg = require('../db/mongo/models/gameModel')
 const CardMg = require('../db/mongo/models/cardModel')
-const UserPg = require('../db/postGres/models/userPostgresModel')
-const CardPg = require('../db/postGres/models/cardPostgresModel')
-const GamePg = require('../db/postGres/models/gamePostgresModel')
-const HandPg = require('../db/postGres/models/handPostgresModel')
 
-getAll = async (req, res, next) => {
+const getAll = async (req, res, next) => {
     try {
         const games = await GameMg.find({})
-        res.send(games)
+        return res.status(200).send(games)
     } catch (error) {
-        console.log(error.message)
+        return next(createError(500, error.message))
     }
 }
 
 const create = async (req, res, next) => {
-    const userId = req.user._id
-    const { players } = req.body
-
-    const user = await UserMg.findById(userId)
-    if (!user) {
-        return next(createError(404, 'user not found'))
-    }
-    if (user.games.length >= 5 && user.status == 'base') {
-        return next(createError(400, 'game limit reached'))
-    }
-
     try {
+        const userId = req.user._id
+        const { players } = req.body
+
+        const user = await UserMg.findById(userId)
+        if (!user) {
+            return next(createError(404, 'user not found'))
+        }
+
+        if (user.games.length >= 5 && user.status == 'base') {
+            return next(createError(400, 'game limit reached'))
+        }
+
         let discard = []
         const deck = await CardMg.find({})
         const numberCards = await CardMg.find({ type: 'number' })
@@ -53,35 +50,39 @@ const create = async (req, res, next) => {
 
         user.games.push(game._id)
         await user.save({ validateBeforeSave: false })
-        res.send(game)
+        return res.status(201).send(game)
     } catch (error) {
-        console.log(error.message)
+        return next(createError(500, error.message))
     }
 }
 
 const startGame = async (req, res, next) => {
-    const { id } = req.params
-    const userId = req.user._id
-
-    const user = await UserMg.findById(userId)
-    if (!user) {
-        return next(createError(404, 'user not found'))
-    }
-    const game = await GameMg.findById(id).populate('players')
-    if (!game) {
-        return next(createError(404, 'game not found'))
-    }
-    if (game.owner.toString() !== userId.toString()) {
-        return next(createError(403, 'only the owner can start the game'))
-    }
-    if (game.status === 'ended') {
-        return next(createError(400, 'game already ended'))
-    }
-    if (game.status === 'started') {
-        return next(createError(400, 'game already started'))
-    }
-
     try {
+        const { id } = req.params
+        const userId = req.user._id
+
+        const user = await UserMg.findById(userId)
+        if (!user) {
+            return next(createError(404, 'user not found'))
+        }
+
+        const game = await GameMg.findById(id).populate('players')
+        if (!game) {
+            return next(createError(404, 'game not found'))
+        }
+
+        if (game.owner.toString() !== userId.toString()) {
+            return next(createError(403, 'only the owner can start the game'))
+        }
+
+        if (game.status === 'ended') {
+            return next(createError(400, 'game already ended'))
+        }
+
+        if (game.status === 'started') {
+            return next(createError(400, 'game already started'))
+        }
+
         game.status = 'started'
         game.turn = 0
         game.direction = 'clockwise'
@@ -89,33 +90,28 @@ const startGame = async (req, res, next) => {
 
         for (let i = 0; i < game.players.length; i++) {
             const player = await UserMg.findById(game.players[i])
-            console.log(
-                '🚀 ~ file: gameController.js:92 ~ startGame ~ player:',
-                player
-            )
 
             const hand = await HandMg.create({
                 player: player._id,
                 cards: [],
                 game: game._id,
             })
-            
+
             for (let j = 0; j < 7; j++) {
                 const randomIndex = Math.floor(Math.random() * game.deck.length)
-                console.log("🚀 ~ file: gameController.js:105 ~ startGame ~ randomIndex:", randomIndex)
                 const card = game.deck[randomIndex]
                 hand.cards.push(card)
                 game.deck.splice(randomIndex, 1)
             }
-            console.log("🚀 ~ file: gameController.js:102 ~ startGame ~ hand:", hand)
+
             await hand.save({ validateBeforeSave: false })
             await player.save({ validateBeforeSave: false })
         }
+
         await game.save({ validateBeforeSave: false })
-        res.send(game)
+        return res.status(200).send(game)
     } catch (error) {
-        console.error(error)
-        return next(createError(500))
+        return next(createError(500, error.message))
     }
 }
 
